1437b4483718ab458f2dddbe1f08bc491e97d546
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-04-23 14:14:05 +0300

    game going on

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,36 @@
-function continueGame(){
+// Capture button press
+document.addEventListener("keyup", function (event) {
+    const playerPressed = event.key;
+
+    // get the expected to press
+    const currentalphabetElement = document.getElementById("current-alphabet").innerText.toLocaleLowerCase();
+    if(playerPressed === currentalphabetElement){
+
+        // update score
+
+
+        // start a new round
+        continueGame();
+        removeBackgroundColorById(currentalphabetElement);
+    }
+    else{
+        // score will be decrease.
+        console.log("oh no! you lost 1 life.")
+    }
+    
+})
+
+
+function continueGame() {
     let alphabet = getRandomAlphabet();
     let currentElphabetElement = document.getElementById("current-alphabet");
     currentElphabetElement.innerText = alphabet;
     // set background color
     setBackgroundColorById(alphabet);
+
 }
 
-function play(){
+function play() {
     hideElementByIt("home");
     showElementByIt("play-ground");
     continueGame();
