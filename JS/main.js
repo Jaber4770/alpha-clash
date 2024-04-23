2c4dba41d8e1bc9614c99938a052c33f5dc27ab7
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-04-23 22:15:39 +0300

    working on game

--- JS/main.js
+++ JS/main.js
@@ -4,20 +4,37 @@ document.addEventListener("keyup", function (event) {
 
     // get the expected to press
     const currentalphabetElement = document.getElementById("current-alphabet").innerText.toLocaleLowerCase();
-    if(playerPressed === currentalphabetElement){
-
+    if (playerPressed === currentalphabetElement) {
         // update score
+        let currentScore = getTextElementById("current-Score");
+        updateScore = currentScore + 1;
+        setTextElementById("current-Score", updateScore);
+        console.log(updateScore);
 
+        // if socre cross 5, everytime add life 1;
+        if (currentScore === 5 || currentScore === 10 || currentScore === 15 || currentScore === 20 || currentScore === 25 || currentScore === 30 || currentScore === 35) {
+            let currentLife = getTextElementById("life");
+            let updateLife = currentLife + 1;
+            setTextElementById("life", updateLife);
+        }
 
         // start a new round
         continueGame();
         removeBackgroundColorById(currentalphabetElement);
     }
-    else{
+    else {
         // score will be decrease.
-        console.log("oh no! you lost 1 life.")
+        let currentLife = getTextElementById("life");
+        if (currentLife === 0) {
+            gameOver();
+        }
+        else {
+            let currentLife = getTextElementById("life");
+            let updateLife = currentLife - 1;
+            setTextElementById("life", updateLife);
+        }
     }
-    
+
 })
 
 
@@ -31,7 +48,13 @@ function continueGame() {
 }
 
 function play() {
-    hideElementByIt("home");
-    showElementByIt("play-ground");
+    hideElementById("home");
+    showElementById("play-ground");
     continueGame();
+}
+
+function gameOver() {
+    hideElementById("play-ground");
+    showElementById("final-score");
+
 }
--- JS/utility.js
+++ JS/utility.js
@@ -1,26 +1,46 @@
-function hideElementByIt(elementId){
-        const element = document.getElementById(elementId);
-        element.classList.add("hidden");
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add("hidden");
 }
-function showElementByIt(elementId){
-        const element = document.getElementById(elementId);
-        element.classList.remove("hidden");
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove("hidden");
 }
-function setBackgroundColorById(elementId){
+function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add("bg-orange-400", "font-bold");
 }
-function removeBackgroundColorById(elementId){
+function removeBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove("bg-orange-400", "font-bold");
 }
+function getTextElementById(elementId) {
+    let element = parseInt(document.getElementById(elementId).innerText);
+    return element;
+}
+function setTextElementById(elementId, value) {
+    let element = document.getElementById(elementId);
+    element.innerText = value;
 
-function getRandomAlphabet(){
+}
+function getRandomAlphabet() {
     const alphabetString = "abcdefghijklmnopqrstuvwxyz";
     let singleAlphabet = alphabetString.split('');
     // get Random Number
-    const randomNumber = Math.random()*25;
+    const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
     const alphabet = singleAlphabet[index];
     return alphabet;
 }
+
+
+
+
+
+
+
+
+
+
+
+
